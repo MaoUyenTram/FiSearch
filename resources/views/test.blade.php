c46d83826a17353fc25349b223b588e21ae14465
Bavo <b4vom4es@gmail.com>
2018-11-08 10:44:45 +0100

    PSR-2 styling

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -1,71 +1,59 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: maoke
- * Date: 5/11/2018
- * Time: 13:10
- */
+    /**
+     * Created by PhpStorm.
+     * User: maoke
+     * Date: 5/11/2018
+     * Time: 13:10
+     */
 
-use Smalot\PdfParser\Parser;
-//src = https://stackoverflow.com/questions/2123236/count-how-often-the-word-occurs-in-the-text-in-php
-// de inefficient windows versie
-// Parse pdf file and build necessary objects.
-$parser = new Parser();
-$pdf    = $parser->parseFile('pdf\randomeindwerk.pdf');
+    use Smalot\PdfParser\Parser;
+    //src = https://stackoverflow.com/questions/2123236/count-how-often-the-word-occurs-in-the-text-in-php
+    // Parse pdf file and build necessary objects.
+    $parser = new Parser();
+    $pdf    = $parser->parseFile('pdf/randomeindwerk.pdf');
 
+    // Retrieve all pages from the pdf file.
+    $pages  = $pdf->getPages();
+    $a = 0;
 
-// Retrieve all pages from the pdf file.
-$pages  = $pdf->getPages();
-$a = 0;
-
-
-// Loop over each page to extract text.
-//foreach ($pages as $page) {
-
-    if ($a < 10){
-    $words = utf8_str_word_count($pdf->getText(), 1);
-    if (count($words) > 1){
-    $new_str = preg_replace('~[\\\\/:*?"<>|]~','', implode(" ",$words));
-    $bigstr = explode(" ",$new_str);
-    $frequency = array_count_values($words);
-
-    arsort($frequency);
-    print_r($frequency);
-    //echo $page->getText();
-    }
-    }
-
-    $a ++;
-//    }
-$details  = $pdf->getDetails();
-
-// Loop over each property to extract values (string or array).
-foreach ($details as $property => $value) {
-    if (is_array($value)) {
-        $value = implode(', ', $value);
-    }
-    echo $property . ' => ' . $value . "\n";
-}
-
-
-function utf8_str_word_count($string, $format = 0, $charlist = null)
-{
-    $result = array();
-
-    if (preg_match_all('~[\p{L}\p{Mn}\p{Pd}\'\x{2019}' . preg_quote($charlist, '~') . ']+~u', $string, $result) > 0)
-    {
-        if (array_key_exists(0, $result) === true)
-        {
-            $result = $result[0];
+    if ($a < 10) {
+        $words = utf8_str_word_count($pdf->getText(), 1);
+        if (count($words) > 1) {
+            $new_str = preg_replace('~[\\\\/:*?"<>|]~', '', implode(" ", $words));
+            $bigstr = explode(" ", $new_str);
+            $frequency = array_count_values($words);
+            arsort($frequency);
+            print_r($frequency);
         }
     }
+    $a ++;
+    $details  = $pdf->getDetails();
 
-    if ($format == 0)
-    {
-        $result = count($result);
+    // Loop over each property to extract values (string or array).
+    foreach ($details as $property => $value) {
+        if (is_array($value)) {
+            $value = implode(', ', $value);
+        }
+        echo $property . ' => ' . $value . "\n";
     }
 
-    return $result;
-}
+
+    function utf8_str_word_count($string, $format = 0, $charlist = null)
+    {
+        $result = array();
+        if (preg_match_all('~[\p{L}\p{Mn}\p{Pd}\'\x{2019}' . preg_quote($charlist, '~') . ']+~u', $string, $result) > 0) {
+            if (array_key_exists(0, $result) === true)
+            {
+                $result = $result[0];
+            }
+        }
+
+        if ($format == 0)
+        {
+            $result = count($result);
+        }
+
+        return $result;
+    }
 
 ?>
