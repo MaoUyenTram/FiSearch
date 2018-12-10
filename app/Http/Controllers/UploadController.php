62fa23ab36fa7a849f7421890bfbd6ebe4021d2e
mao uyen tram <maoketram@hotmail.com>
2018-12-10 18:23:57 +0100

    merging img api and textanalysing

--- app/Http/Controllers/UploadController.php
+++ app/Http/Controllers/UploadController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Smalot\PdfParser\Parser;
 use GuzzleHttp\Client;
+use ConvertApi\ConvertApi;
 
 class UploadController extends Controller
 {
@@ -18,11 +19,23 @@ class UploadController extends Controller
     {
         $parser = new Parser();
         $pdf = $parser->parseFile($request->file('pdf'));
-        $name = $request->file('pdf')->getClientOriginalName();
-        $request->file('pdf')->move(public_path('pdf'),$name);
-        $text = preg_replace('~[\\\\/:*?"<>|]~', '', $pdf->getText());
-        $limited_text = substr($text, 0, 4900);
-
+        $request->file('pdf')->move(public_path('pdf'),"file.pdf");
+        //$text = mb_strtolower($pdf->getText());
+        $text = str_replace('.', '', mb_strtolower($pdf->getText()));
+        //$text = mb_convert_encoding($text, "UTF-8");
+        $cutf = strpos($text, "index");
+        if (!$cutf) {
+            $cutf = strpos($text, "table of content");
+        }
+        if (!$cutf) {
+            $cutf = strpos($text, "inhouds");
+        }
+        if (!$cutf) {
+            return "error no table of contents/inhoudstafel/index";
+        }
+        $rftext = substr($text,$cutf);
+        $limited_text = substr($rftext, 5,4800);
+        //return $limited_text;
         $client = new Client();
 
         $response = $client->request('POST', 'https://westeurope.api.cognitive.microsoft.com/text/analytics/v2.0/KeyPhrases', [
@@ -41,7 +54,24 @@ class UploadController extends Controller
                 ]
             ]
         ]);
-        return json_decode($response->getBody(),true);
+        $tags = json_decode($response->getBody(),true);
+
+        $secret = "lTv3cVFVsvFRY3Nf";
+        ConvertApi::setApiSecret($secret);
+        $result = ConvertApi::convert(
+            'jpg',
+            [
+                'File' => 'pdf/file.pdf',
+                'PageRange' => '1',
+            ], 'pdf');
+        $result->getFile()->save('pdf/test.jpg');
+        $img = $result->getFile()->getUrl();
+
+        return array(
+            'tags' => $tags,
+            'img' =>  $img,
+        );
+
 
     }
 }
