c21877c1fc0afb1737a201eb751a89b6d666a5c8
fu6as <alvi.istamalov@gmail.com>
2018-12-19 18:17:00 +0100

    AUTH implementation

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,6 @@ Route::get('/', function() {
 
 Route::get('/annotate', 'AnnotationController@displayForm');
 Route::post('/annotate', 'AnnotationController@annotateImage');
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
